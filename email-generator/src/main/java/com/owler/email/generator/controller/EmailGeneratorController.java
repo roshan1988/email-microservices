d40b5ac2337f7801ae1ed251abaf18df59b6f3b4
Roshan Alexander <roshanalexander88@gmail.com>
2017-01-09 21:34:08 +0530

    no message

--- email-generator/src/main/java/com/owler/email/generator/controller/EmailGeneratorController.java
+++ email-generator/src/main/java/com/owler/email/generator/controller/EmailGeneratorController.java
@@ -1,8 +1,11 @@
 package com.owler.email.generator.controller;
 
+import java.util.Calendar;
 import java.util.List;
+import java.util.concurrent.atomic.LongAdder;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.GaugeService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,18 +21,52 @@ import com.owler.email.generator.entity.EmailEntity;
 @CrossOrigin
 @RequestMapping("/generator")
 public class EmailGeneratorController {
-	
+
 	@Autowired
 	GeneratorComponent generatorComponent;
-	
+
 	@Autowired
 	TransmitterComponent transmitterComponent;
 
-	@RequestMapping(value="/generate" , method = RequestMethod.POST)
-	long generateAndSendEmail(@RequestBody CheckInRecord checkInRecord){
+	@Autowired
+	GaugeService gaugeService;
+
+	TPMCounter tpm = new TPMCounter();
+
+	@RequestMapping(value = "/generate", method = RequestMethod.POST)
+	long generateAndSendEmail(@RequestBody CheckInRecord checkInRecord) {
 		List<EmailEntity> emailList = generatorComponent.generateEmail(checkInRecord);
 		transmitterComponent.sendEmailToTransmitter(emailList);
+		tpm.increment();
+		gaugeService.submit("tpm", tpm.count.intValue());
 		return emailList.size();
 	}
-	
+
+	class TPMCounter {
+		LongAdder count;
+		Calendar expiry = null;
+
+		TPMCounter() {
+			reset();
+		}
+
+		void reset() {
+			count = new LongAdder();
+			expiry = Calendar.getInstance();
+			expiry.add(Calendar.MINUTE, 1);
+		}
+
+		boolean isExpired() {
+			return Calendar.getInstance().after(expiry);
+		}
+
+		void increment() {
+			if (isExpired()) {
+				reset();
+			}
+			count.increment();
+		}
+
+	}
+
 }
