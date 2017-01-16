053500f0e68e35777702adce6fb9f3a567388113
Roshan Alexander <roshanalexander88@gmail.com>
2017-01-16 19:13:10 +0530

    no message

--- deploy-marathon-services.sh
+++ deploy-marathon-services.sh
@@ -1,7 +1,27 @@
+
+function dockip() {   
+docker inspect --format '{{ .NetworkSettings.IPAddress }}' "$@"; 
+}
+
+function editEnvConfig() { 
+  jq '.env."'$1'"="'$2'"'
+}
+
 cd marathon-applications
-curl -X POST http://localhost:8080/v2/apps -d @email-event-checkin.json -H "Content-type: application/json"
-curl -X POST http://localhost:8080/v2/apps -d @email-event-checkin-apigateway.json -H "Content-type: application/json"
-curl -X POST http://localhost:8080/v2/apps -d @email-generator.json -H "Content-type: application/json"
-curl -X POST http://localhost:8080/v2/apps -d @email-transmitter.json -H "Content-type: application/json"
-curl -X POST http://localhost:8080/v2/apps -d @email-lifecycle-manager.json -H "Content-type: application/json"
+
+configIp=$(dockip configServer)
+eurekaIp=$(dockip eurekaServer)
+
+cat email-event-checkin.json | editEnvConfig "spring.cloud.config.uri" "http://${configIp}:8888" | editEnvConfig "eureka.client.serviceUrl.defaultZone" "http://${eurekaIp}:8761/eureka/" > email-event-checkin-modified.json
+echo "Posting Application to Marathon : Event Checkin Service"
+curl -X POST http://localhost:8080/v2/apps -d @email-event-checkin-modified.json -H "Content-type: application/json"
+cat email-event-checkin-apigateway.json | editEnvConfig "spring.cloud.config.uri" "http://${configIp}:8888" | editEnvConfig "eureka.client.serviceUrl.defaultZone" "http://${eurekaIp}:8761/eureka/" > email-event-checkin-apigateway-modified.json
+echo "Posting Application to Marathon : Event Checkin API Service"
+curl -X POST http://localhost:8080/v2/apps -d @email-event-checkin-apigateway-modified.json -H "Content-type: application/json"
+cat email-generator.json | editEnvConfig "spring.cloud.config.uri" "http://${configIp}:8888" | editEnvConfig "eureka.client.serviceUrl.defaultZone" "http://${eurekaIp}:8761/eureka/" > email-generator-modified.json
+echo "Posting Application to Marathon : Email Generator Service"
+curl -X POST http://localhost:8080/v2/apps -d @email-generator-modified.json -H "Content-type: application/json"
+cat email-transmitter.json | editEnvConfig "spring.cloud.config.uri" "http://${configIp}:8888" | editEnvConfig "eureka.client.serviceUrl.defaultZone" "http://${eurekaIp}:8761/eureka/" > email-transmitter-modified.json
+echo "Posting Application to Marathon : Email Transmitter Service"
+curl -X POST http://localhost:8080/v2/apps -d @email-transmitter-modified.json -H "Content-type: application/json"
 cd ..
