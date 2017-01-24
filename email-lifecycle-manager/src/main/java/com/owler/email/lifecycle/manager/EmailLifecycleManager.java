00be4f920883341c648bc0165019492a8528e320
Roshan Alexander <roshanalexander88@gmail.com>
2017-01-24 11:59:19 +0530

    no message

--- email-lifecycle-manager/src/main/java/com/owler/email/lifecycle/manager/EmailLifecycleManager.java
+++ email-lifecycle-manager/src/main/java/com/owler/email/lifecycle/manager/EmailLifecycleManager.java
@@ -6,22 +6,35 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
+import com.owler.email.lifecycle.manager.deploymentengine.DeploymentEngine;
 import com.owler.email.lifecycle.manager.metricscollector.MetricsCollector;
 @EnableDiscoveryClient
-@SpringBootApplication
 @EnableAutoConfiguration
-public class EmailLifecycleManager implements CommandLineRunner {
-	
-	@Autowired
-	MetricsCollector metricsCollector;
+public class EmailLifecycleManager {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(EmailLifecycleManager.class, args);
+		try {
+			HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			HttpEntity<String> entity = new HttpEntity<String>("{ \"instances\" : \""+ 2 +"\" }", headers);
+			RestTemplate restTemplate = new RestTemplate();
+			ResponseEntity<String> postResponse = restTemplate.exchange("http://192.168.99.100:8080/v2/apps//email-event-checkin-apigateway", HttpMethod.PUT,
+					entity, String.class);
+			if (postResponse.getStatusCode() != HttpStatus.OK) {
+				System.out.println(
+						"Error upscaling the instance :  Error Code : " + postResponse.getStatusCode());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	@Override
-	public void run(String... arg0) throws Exception {
-		metricsCollector.start();
-	}
 }
