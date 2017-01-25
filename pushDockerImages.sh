7c1ac94a7b1a53cb943fc8b02ac510e268f7c131
Roshan Alexander <roshanalexander88@gmail.com>
2017-01-26 02:02:44 +0530

    no message

--- pushDockerImages.sh
+++ pushDockerImages.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-docker push roshan1988/email-eureka
+#docker push roshan1988/email-eureka
 # docker push roshan1988/email-event-checkin
 # docker push roshan1988/email-event-checkin-apigateway
 # docker push roshan1988/email-generator
 # docker push roshan1988/email-transmitter
-docker push roshan1988/email-lifecycle-manager
+# docker push roshan1988/email-lifecycle-manager
