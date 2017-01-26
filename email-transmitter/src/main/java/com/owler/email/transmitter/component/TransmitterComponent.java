39771884108392b3a86b64ae1563fae14f502d16
Roshan Alexander <roshanalexander88@gmail.com>
2017-01-26 21:24:57 +0530

    Added fake smtp support

--- email-transmitter/src/main/java/com/owler/email/transmitter/component/TransmitterComponent.java
+++ email-transmitter/src/main/java/com/owler/email/transmitter/component/TransmitterComponent.java
@@ -2,7 +2,10 @@ package com.owler.email.transmitter.component;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Component;
 
 import com.owler.email.transmitter.entity.EmailEntity;
@@ -10,16 +13,23 @@ import com.owler.email.transmitter.entity.EmailEntity;
 @RefreshScope
 @Component
 public class TransmitterComponent {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(TransmitterComponent.class);
 
-	public TransmitterComponent(){
-		
-	}
-	
+	@Autowired
+	MailSender mailSender;
+
 	public void transmitEmail(EmailEntity emailEntity) {
 		logger.info("Transmitting Email : " + emailEntity.getId());
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setText(emailEntity.getContent());
+		message.setTo(emailEntity.getToAddress());
+		message.setFrom("owlerinsightl@owler.com");
+		try {
+			mailSender.send(message);
+		} catch (Exception e) {
+			logger.error("Error sending mail with id : " + emailEntity.getId(), e);
+		}
 	}
-	
-}
 
+}
